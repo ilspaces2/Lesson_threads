4d41f68ad5221232f6ffb81220767e29170f2b00
I_M <moskva.88@mail.ru>
2022-05-17 17:29:20 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,32 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        return memory.computeIfPresent(model.getId(),
+                (key, value) -> {
+                    Base stored = memory.get(key);
+                    if (stored.getVersion() != model.getVersion()) {
+                        throw new OptimisticException("Versions are not equal");
+                    }
+                    stored.setName(model.getName());
+                    return stored;
+                }) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+
+    public Map<Integer, Base> getMap() {
+        return Map.copyOf(memory);
+    }
+}
---
+++ src/test/java/ru/job4j/cache/CacheTest.java
@@ -0,0 +1,70 @@
+package ru.job4j.cache;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class CacheTest {
+
+    @Test
+    public void whenAddToCache() {
+        Cache cache = new Cache();
+        Base user1 = new Base(1, 1);
+        Base user2 = new Base(2, 1);
+        Base user3 = new Base(3, 1);
+        user1.setName("1");
+        user2.setName("2");
+        user3.setName("3");
+        cache.add(user1);
+        cache.add(user2);
+        cache.add(user3);
+        assertThat(Map.of(1, user1, 2, user2, 3, user3), is(cache.getMap()));
+    }
+
+    @Test
+    public void whenUpdateCache() {
+        Cache cache = new Cache();
+        Base user1 = new Base(1, 1);
+        Base user2 = new Base(1, 1);
+        user1.setName("old name");
+        cache.add(user1);
+        user2.setName("new name");
+        cache.update(user2);
+        assertThat(user2.getName(), is(cache.getMap().get(1).getName()));
+    }
+
+    @Test
+    public void whenDeleteCache() {
+        Cache cache = new Cache();
+        Base user1 = new Base(1, 1);
+        user1.setName("1");
+        cache.add(user1);
+        cache.delete(user1);
+        assertTrue(cache.getMap().isEmpty());
+    }
+
+    @Test(expected = OptimisticException.class)
+    public void whenUpdateCacheAndDifferentVersions() {
+        Cache cache = new Cache();
+        Base user1 = new Base(1, 1);
+        Base user2 = new Base(1, 2);
+        cache.add(user1);
+        cache.update(user2);
+    }
+
+    @Test
+    public void whenAddThenTrue() {
+        Cache cache = new Cache();
+        assertTrue(cache.add(new Base(0, 0)));
+    }
+
+    @Test
+    public void whenUpdateThenTrue() {
+        Cache cache = new Cache();
+        cache.add(new Base(0, 0));
+        assertTrue(cache.update(new Base(0, 0)));
+    }
+}
